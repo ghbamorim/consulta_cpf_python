ddcdd9d68ee7d4d78fc52a684d94bfcf7ccaff7d
Gustavo Amorim <gamorim@informa.com.br>
2021-08-31 09:28:10 -0300

    jwt auth implemented

--- src/app.py
+++ src/app.py
@@ -5,40 +5,84 @@ from flask_restful import Api
 import jwt
 import datetime
 from fake_db import Db
+from functools import wraps
 
 app = Flask(__name__)
 
-app.config['SECRET'] = 'SECRET'
+app.config['SECRET'] = 'SECRET_KEY'
+app.config['TOKEN_EXPIRES_IN'] = 30  #minutes
+
+db = Db()
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return jsonify({'message': 'Missing token'}), 401
+
+        jwt_data = jwt.decode(token, app.config['SECRET'], algorithms="HS256")
+
+        try:
+            user = db.find_by_id(jwt_data['public_id'])
+            if not user:
+                return jsonify({'message': 'Inválid token'}), 401
+        except:
+            return jsonify({'message': 'Inválid token'}), 401
+        return f(user, *args, **kwargs)
+
+    return decorated
+
 
 @app.route("/login", methods=['GET'])
 def login():
-  auth = request.authorization
-  db = Db()
-  db.init_fake_db()
-  user = db.find_user(1, '123456')
-  if user:
-    token = jwt.encode({'public_id' : 'public_id', 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET']), 200
-    return jsonify({'token' : token})
-  return jsonify({'message': 'invalid login or password'}), 401
+    auth = request.authorization
+    user_name = auth.username
+    password = auth.password
+
+    user = db.find_user(user_name, password)
+    if user:
+        token = jwt.encode(
+            {
+                'public_id':
+                user.id,
+                'exp':
+                datetime.datetime.utcnow() +
+                datetime.timedelta(minutes=app.config['TOKEN_EXPIRES_IN'])
+            },
+            app.config['SECRET'],
+            algorithm="HS256")
+        return jsonify({'token': token})
+    return jsonify({'message': 'invalid login or password'}), 401
+
 
 @app.route("/dadosCpf", methods=['GET'])
-def getCpf():
+@token_required
+def getCpf(user):
     body = request.get_json()
     print(body)
     try:
 
-      cpf_params = Cpf_params()
-      cpf_params.client_id = '8ddc46f2-f6a3-4077-9e04-74b55de934a5'
-      cpf_params.client_secret = '06d4aaac-1412-45f6-bd7c-38b2bef0d706'
-      cpf_params.user_cpf = '00993162428'
-      cpf_params.cpfs_for_query = body["cpf"]
+        cpf_params = Cpf_params()
+        cpf_params.client_id = '8ddc46f2-f6a3-4077-9e04-74b55de934a5'
+        cpf_params.client_secret = '06d4aaac-1412-45f6-bd7c-38b2bef0d706'
+        cpf_params.user_cpf = '00993162428'
+        cpf_params.cpfs_for_query = body["cpf"]
 
-      serpro_intf = Serpro_intf()
-      return json.dumps(serpro_intf.consultaCpf(cpf_params))
+        serpro_intf = Serpro_intf()
+        return json.dumps(serpro_intf.consultaCpf(cpf_params))
 
     except Exception as e:
         print('Erro:', e)
-        return Response(json.dumps( {'error': str(e)}), status=500, mimetype='application/json')
+        return Response(json.dumps({'error': str(e)}),
+                        status=500,
+                        mimetype='application/json')
+
 
 #if __name__ == '__main__':
 app.run(debug=True)
--- src/fake_db.py
+++ src/fake_db.py
@@ -1,32 +1,41 @@
 from users import Users
 
+
 class Db():
-  db = []
-  
-  def init_fake_db(self):  
-    new_user = Users()
-    new_user.id = 1
-    new_user.cpf = '72002557853'
-    new_user.name = 'user01'
-    new_user.password = '123456'
+    db = []
 
-    self.db.append(new_user)
+    def init_fake_db(self):
+        new_user = Users()
+        new_user.id = 1
+        new_user.cpf = '72002557853'
+        new_user.name = 'user01'
+        new_user.password = '123456'
 
-    new_user = Users()
-    new_user.id = 2
-    new_user.cpf = '26616776824'
-    new_user.name = 'user02'
-    new_user.password = '123456'
+        self.db.append(new_user)
 
-    self.db.append(new_user)
+        new_user = Users()
+        new_user.id = 2
+        new_user.cpf = '26616776824'
+        new_user.name = 'user02'
+        new_user.password = '123456'
 
-    new_user = Users()
-    new_user.id = 3
-    new_user.cpf = '82272182100'
-    new_user.name = 'user03'
-    new_user.password = '123456'
+        self.db.append(new_user)
 
-    self.db.append(new_user)
+        new_user = Users()
+        new_user.id = 3
+        new_user.cpf = '82272182100'
+        new_user.name = 'user03'
+        new_user.password = '123456'
 
-  def find_user(self, user_id, password):
-    return next((user for user in self.db if user.id == user_id and user.password == password), None)
+        self.db.append(new_user)
+
+    def find_by_id(self, user_id):
+        return next((user for user in self.db if user.id == user_id), None)
+
+    def find_user(self, user_name, password):
+        return next((user for user in self.db
+                     if user.name == user_name and user.password == password),
+                    None)
+
+    def __init__(self):
+        self.init_fake_db()
